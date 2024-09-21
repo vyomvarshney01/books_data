40acd63516c0cbb611a03624fb846f719b988bb8
vyomvarshney01 <108329558+vyomvarshney01@users.noreply.github.com>
2024-09-21 20:32:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+const cors = require('cors');
+
+const { getAllBooks, getBookById } = require('./controllers');
+app.use(cors());
+app.use(express.json());
+
+// Endpoint to get all books
+app.get('/books', async (req, res) => {
+  try {
+    const books = getAllBooks();
+    if (books.length === 0) {
+      res.status(400).json('No book details found');
+    }
+    res.status(200).json({ books });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Endpoint to get book detalls by id
+app.get('/books/details/:id', async (req, res) => {
+  try {
+    let book = getBookById(parseInt(req.params.id));
+    if (book.length === 0) {
+      res.status(400).json('No book detail found by this id');
+    }
+    res.status(200).json({
+      book,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = { app };
---
+++ tests/api.test.js
@@ -0,0 +1,96 @@
+const { app } = require('../index');
+const request = require('supertest');
+const http = require('http');
+const { getAllBooks } = require('../controllers');
+
+jest.mock('../controllers', () => ({
+  ...jest.requireActual('../controllers'),
+  getAllBooks: jest.fn(),
+}));
+
+let server;
+
+beforeAll(async () => {
+  server = http.createServer(app);
+  server.listen(3001);
+});
+
+afterAll(async () => {
+  server.close();
+});
+
+describe('Controller Function tests', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return all games', () => {
+    let mockedBooks = [
+      {
+        bookId: 1,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        genre: 'Fiction',
+      },
+      {
+        bookId: 2,
+        title: '1984',
+        author: 'George Orwell',
+        genre: 'Dystopian',
+      },
+      {
+        bookId: 3,
+        title: 'The Great Gatsby',
+        author: 'F. Scott Fitzgerald',
+        genre: 'Classic',
+      },
+    ];
+    getAllBooks.mockReturnValue(mockedBooks);
+    let result = getAllBooks();
+    expect(result).toEqual(mockedBooks);
+    expect(result.length).toBe(3);
+  });
+});
+
+describe('API Endpoint tests', () => {
+  it('GET /books should get all books', async () => {
+    const res = await request(server).get('/books');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      books: [
+        {
+          bookId: 1,
+          title: 'To Kill a Mockingbird',
+          author: 'Harper Lee',
+          genre: 'Fiction',
+        },
+        {
+          bookId: 2,
+          title: '1984',
+          author: 'George Orwell',
+          genre: 'Dystopian',
+        },
+        {
+          bookId: 3,
+          title: 'The Great Gatsby',
+          author: 'F. Scott Fitzgerald',
+          genre: 'Classic',
+        },
+      ],
+    });
+    expect(res.body.books.length).toBe(3);
+  });
+
+  it('GET /books/details/:id should get an book by ID', async () => {
+    const res = await request(server).get('/books/details/1');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      book: {
+        bookId: 1,
+        title: 'To Kill a Mockingbird',
+        author: 'Harper Lee',
+        genre: 'Fiction',
+      },
+    });
+  });
+});
